9a5f44b1341f483da15950b7822e7870526e080a
Tom Oram <tom.oram@armakuni.com>
2019-02-19 14:31:51 +0000

    Refactor: Extract methods for clarity

--- bowling_game.py
+++ bowling_game.py
@@ -14,17 +14,23 @@ class BowlingGame:
         roll_index = 0
 
         for frame in range(TOTAL_FRAMES):
-            if self.rolls[roll_index] == ALL_PINS:
-                total_score += ALL_PINS + self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+            if self._is_strike(roll_index):
+                total_score += ALL_PINS + self._strike_bonus(roll_index)
                 roll_index += 1
             elif self._is_spare(roll_index):
-                total_score += 10 + self._spare_bonus(roll_index)
+                total_score += ALL_PINS + self._spare_bonus(roll_index)
                 roll_index += 2
             else:
                 total_score += self._frame_score(roll_index)
                 roll_index += 2
 
         return total_score
+
+    def _is_strike(self, roll_index):
+        return self.rolls[roll_index] == ALL_PINS
+
+    def _strike_bonus(self, roll_index):
+        return self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
 
     def _is_spare(self, roll_index):
         return self._frame_score(roll_index) == ALL_PINS
