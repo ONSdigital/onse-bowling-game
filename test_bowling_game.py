5fb60c3b38475a310db115e975626a1d0508ebc3
Tom Oram <tom.oram@armakuni.com>
2019-02-19 14:09:18 +0000

    Refactor: Extract pytest fixture

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -1,17 +1,20 @@
+import pytest
+
 from bowling_game import BowlingGame
 
 
-def test_gutter_game():
-    game = BowlingGame()
+@pytest.fixture
+def game():
+    return BowlingGame()
 
+
+def test_gutter_game(game):
     roll_many(game, count=20, pins=0)
 
     assert game.score() == 0
 
 
-def test_all_ones():
-    game = BowlingGame()
-
+def test_all_ones(game):
     roll_many(game, count=20, pins=1)
 
     assert game.score() == 20
