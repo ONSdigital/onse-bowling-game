cdff11f2b8ebb4150c1b7163430047f03aab2e1f
Tom Oram <tom.oram@armakuni.com>
2019-02-19 14:17:25 +0000

    Refactor: Extract constant and method to improve clarity

--- bowling_game.py
+++ bowling_game.py
@@ -1,3 +1,6 @@
+TOTAL_FRAMES = 10
+
+
 class BowlingGame:
     def __init__(self):
         self.rolls = []
@@ -9,8 +12,11 @@ class BowlingGame:
         total_score = 0
         roll_index = 0
 
-        for frame in range(10):
-            total_score += self.rolls[roll_index] + self.rolls[roll_index + 1]
+        for frame in range(TOTAL_FRAMES):
+            total_score += self._frame_score(roll_index)
             roll_index += 2
 
         return total_score
+
+    def _frame_score(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1]
